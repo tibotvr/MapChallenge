1ef32cdd816f016960f49546a38404d8eb9349bd
MAFTOUL OMAR <maftoul.omar@gmail.com>
2018-12-06 21:22:52 +0100

    Add files via upload

---
+++ App.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+[assembly: XamlCompilation(XamlCompilationOptions.Compile)]
+namespace MapAppChallene
+{
+    public partial class App : Application
+    {
+
+        public static double ScreenWidth;
+        public static double ScreenHeight;
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+
+        protected override void OnStart()
+        {
+            // Handle when your app starts
+        }
+
+        protected override void OnSleep()
+        {
+            // Handle when your app sleeps
+        }
+
+        protected override void OnResume()
+        {
+            // Handle when your app resumes
+        }
+    }
+}
---
+++ MapPageCS.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+using Xamarin.Forms.Maps;
+
+namespace MapAppChallene
+{
+    class MapPageCS : ContentPage
+    {
+        public MapPageCS()
+        {
+            var customMap = new CustomMap
+            {
+                MapType = MapType.Street,
+                WidthRequest = App.ScreenWidth,
+                HeightRequest = App.ScreenHeight
+            };
+
+        }
+}
